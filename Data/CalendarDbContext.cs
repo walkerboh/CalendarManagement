efde457d0fc119743779c7a2a855ae3e18c8c0e4
Evan Frisch <walkerboh1213@gmail.com>
2026-01-24 12:41:30 -0500

    Removed fluent API from DB setup

--- Data/CalendarDbContext.cs
+++ Data/CalendarDbContext.cs
@@ -12,28 +12,4 @@ public class CalendarDbContext : DbContext
     public DbSet<DateEvent> DateEvents => Set<DateEvent>();
     public DbSet<WaitingEvent> WaitingEvents => Set<WaitingEvent>();
     public DbSet<RepeatingEvent> RepeatingEvents => Set<RepeatingEvent>();
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<DateEvent>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
-        });
-
-        modelBuilder.Entity<WaitingEvent>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
-        });
-
-        modelBuilder.Entity<RepeatingEvent>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
-            entity.Property(e => e.WeeksOfMonth).HasMaxLength(20);
-        });
-    }
 }
